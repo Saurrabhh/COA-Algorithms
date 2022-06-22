8d25813cf2f150e4c8bdce492851b0a803c4f5ff
Saurrabhh <saurabhkyadav99@gmail.com>
2022-06-22 22:13:02 +0530

    added binary multi algorithm

--- Binary Multiplication.py
+++ Binary Multiplication.py
@@ -3,8 +3,18 @@ from Utils import Dec
 
 def main():
     num1, num2 = map(int, input("Enter two nos: ").split())
-    num1_bin = bin(num1)[2:]
-    num2_bin = bin(num2)[2:]
+    sign = 1
+    if num1 < 0:
+        sign *= -1
+        num1_bin = bin(-num1)[2:]
+    else:
+        num1_bin = bin(num1)[2:]
+
+    if num2 < 0:
+        sign *= -1
+        num2_bin = bin(-num2)[2:]
+    else:
+        num2_bin = bin(num2)[2:]
     print("x = ", num1_bin)
     print("y = ", num2_bin)
     num1_len = len(num1_bin)
@@ -24,6 +34,7 @@ def main():
         print("+", add.rjust(space))
         total += Dec(add)
         count += 1
+    total *= sign
     print('-'*(space + 2))
     print("=",bin(total)[2:])
     print(f"{num1} x {num2} = {total}")
